2ee365367360097e480526e1264976c1e719cb83
pallabez <pallabsonowal999@gmail.com>
2021-12-25 16:11:05 +0530

    Added elevator flood bound limit and refactored storage of position

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ let up = document.querySelectorAll(".up-btn");
 let down = document.querySelectorAll(".down-btn");
 
 let elevator = document.querySelector(".elevator");
-let currFloor = 0;
+let maxFloors = 4;
+
+let floorChange = function(elevator, nextFloor) {
+    elevator.setAttribute("on-Floor", nextFloor);
+    elevator.style.bottom = nextFloor * 10 + 'rem';
+}
 
 for(let i of up) {
     i.addEventListener('click', function(event) {
-        currFloor++;
-        floorPos = currFloor * 10 + 'rem';
-        elevator.style.bottom = floorPos;
+        let currFloor = parseInt(elevator.getAttribute("on-floor"));
+        if(currFloor < maxFloors - 1) floorChange(elevator, currFloor + 1);
     });
 }
 
 for(let i of down) {
     i.addEventListener('click', function(event) {
-        currFloor--;
-        floorPos = currFloor * 10 + 'rem';
-        elevator.style.bottom = floorPos;
+        let currFloor = parseInt(elevator.getAttribute("on-floor"));
+        if(currFloor > 0) floorChange(elevator, currFloor - 1);
     });
 }
